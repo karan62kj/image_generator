a90e306556c90375c343107f10ebf683a921d9ef
karan62kj <48881570+karan62kj@users.noreply.github.com>
2020-06-23 12:06:20 +0530

    Updates

---
+++ precache-manifest.4cdcc1cdf26262854e1d15622c1fa3ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac88773bb8bdc3ba230a3e713d47b77",
+    "url": "/image_generator/index.html"
+  },
+  {
+    "revision": "b944ac296e5eac67fb85",
+    "url": "/image_generator/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "9681e4cf827a21c36b32",
+    "url": "/image_generator/static/css/main.17b32aa6.chunk.css"
+  },
+  {
+    "revision": "b944ac296e5eac67fb85",
+    "url": "/image_generator/static/js/2.e3c32d59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image_generator/static/js/2.e3c32d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9681e4cf827a21c36b32",
+    "url": "/image_generator/static/js/main.5178f7e7.chunk.js"
+  },
+  {
+    "revision": "37560c664d32c5e353e3",
+    "url": "/image_generator/static/js/runtime-main.69ddfc3f.js"
+  }
+]);
